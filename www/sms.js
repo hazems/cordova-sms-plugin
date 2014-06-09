e22277955d199094a020dc813f0d725eda915607
hazems <hazems@apache.org>
2014-06-09 03:28:17 +0300

    Adding iOS and Android implementation

--- www/sms.js
+++ www/sms.js
@@ -1,26 +1,21 @@
-cordova.define("com.jsmobile.plugins.sms.sms", function(require, exports, module) { 
-    
-    var exec = require('cordova/exec');
+var smsExport = {};
 
-    var smsExport = {};
+smsExport.sendMessage = function(messageInfo, successCallback, errorCallback) {
+	var phoneNumber = messageInfo.phoneNumber;
+	var textMessage = messageInfo.textMessage || "Default Text from SMS plugin";
 
-    smsExport.sendMessage = function(messageInfo, successCallback, errorCallback) {
-        var phoneNumber = messageInfo.phoneNumber;
-        var textMessage = messageInfo.textMessage || "Default Text from SMS plugin";
-        
-        if (! phoneNumber) {
-            console.log("Missing Phone Number");
-            
-            errorCallback({
-            	code: "MISSING_PHONE_NUMBER",
-            	message: "Missing Phone number"
-            });
-            
-            return;
-        }
-        
-        exec(successCallback, errorCallback, "Sms", "sendMessage", [phoneNumber, textMessage]);
-    };
-    
-    module.exports = smsExport;
-});
+	if (! phoneNumber) {
+		console.log("Missing Phone Number");
+	
+		errorCallback({
+			code: "MISSING_PHONE_NUMBER",
+			message: "Missing Phone number"
+		});
+	
+		return;
+	}
+
+	cordova.exec(successCallback, errorCallback, "Sms", "sendMessage", [phoneNumber, textMessage]);
+};
+
+module.exports = smsExport;
