65c3b41e4e079687acebfda825b02b2909c04621
hazems <hazems@apache.org>
2014-09-18 06:33:00 +0300

    Enhancing plugin

--- www/sms.js
+++ www/sms.js
@@ -1,21 +1,35 @@
 var smsExport = {};
 
 smsExport.sendMessage = function(messageInfo, successCallback, errorCallback) {
-	var phoneNumber = messageInfo.phoneNumber;
-	var textMessage = messageInfo.textMessage || "Default Text from SMS plugin";
-
-	if (! phoneNumber) {
-		console.log("Missing Phone Number");
-	
-		errorCallback({
-			code: "MISSING_PHONE_NUMBER",
-			message: "Missing Phone number"
-		});
-	
-		return;
-	}
-
-	cordova.exec(successCallback, errorCallback, "Sms", "sendMessage", [phoneNumber, textMessage]);
+    if (messageInfo == null || typeof messageInfo !== 'object') {
+    
+        if (errorCallback) {
+            errorCallback({
+                code: "INVALID_INPUT",
+                message: "Invalid Input"
+            });
+        }
+       
+        return;
+    }
+           
+    var phoneNumber = messageInfo.phoneNumber;
+    var textMessage = messageInfo.textMessage || "Default Text from SMS plugin";
+           
+    if (! phoneNumber) {
+        console.log("Missing Phone Number");
+    
+        if (errorCallback) {
+            errorCallback({
+                code: "MISSING_PHONE_NUMBER",
+                message: "Missing Phone number"
+            });
+        }
+           
+        return;
+    }
+           
+    cordova.exec(successCallback, errorCallback, "Sms", "sendMessage", [phoneNumber, textMessage]);
 };
 
 module.exports = smsExport;
